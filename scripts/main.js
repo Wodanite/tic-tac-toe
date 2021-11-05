db4b346da8ab9a63804830db997ed299b157c74c
Filip <f.golub321@gmail.com>
2021-11-05 18:38:52 +0100

    Create gameboard object

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const gameboard = (()=>{
+    let gameboardFields = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ];
+
+    const showGameboard = () => gameboardFields;
+    return {
+        showGameboard
+    }
+})();
+
+console.log(gameboard.showGameboard());
