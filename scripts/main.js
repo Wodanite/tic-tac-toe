7595034bd80adc58e7d34c615ee9f62abdc60d0c
Filip <f.golub321@gmail.com>
2021-11-06 10:00:46 +0100

    Create players

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const gameboard = (()=>{
+const gameboard = (() => {
     let gameboardFields = [
         ["", "", ""],
         ["", "", ""],
@@ -14,3 +14,16 @@ const gameboard = (()=>{
 })();
 
 console.log(gameboard.showGameboard());
+
+const Player = (name, mark) => {
+    const getName = () => name;
+    const getMark = () => mark;
+
+    return { getName, getMark };
+};
+
+const player1 = Player("Player 1", "X");
+const player2 = Player("Player 2", "O");
+
+console.log(player1.getName());
+console.log(player2.getMark());
