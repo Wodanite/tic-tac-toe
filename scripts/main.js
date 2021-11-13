f0f729d9962d30ac17f45fd898d6af83946ed19a
Filip <f.golub321@gmail.com>
2021-11-13 18:35:46 +0100

    Add check for same marks in a row or column

--- scripts/main.js
+++ scripts/main.js
@@ -22,8 +22,8 @@ const gameboard = (() => {
         let mark = currentPlayer.getMark();
         gameboardFields[y][x] = mark;
     };
-    
-    for (let i = 1; i < 10; i++){
+
+    for (let i = 1; i < 10; i++) {
         let indexString = "box" + i.toString();
         const currentBox = document.querySelector(`#${indexString}`);
         currentBox.addEventListener("click", () => {
@@ -31,13 +31,11 @@ const gameboard = (() => {
             const [y, x] = getCoordinates(list);
 
             if (gameboard.checkIfLegalMove(y, x) == true) {
-                console.log("before move" + gameFlow.turnOfPlayer.getInternalName());
                 gameboard.addMark(y, x);
-                
+
                 displayController.renderGameboard();
+                gameFlow.checkEndOfGame();
                 gameFlow.switchTurn();
-                console.log("after passing switch turn to gameboard"+gameFlow.turnOfPlayer.getInternalName());
-                console.log("my new method " +gameFlow.getPlayerOnTurn().getInternalName());
             }
 
         });
@@ -71,26 +69,51 @@ const gameFlow = (() => {
     let endOfGame = false;
     let turnOfPlayer = player1;
     const switchTurn = () => {
-        console.log("before switching turn"+turnOfPlayer.getInternalName());
         if (turnOfPlayer.getInternalName() == "player1") {
             turnOfPlayer = player2;
         } else {
             turnOfPlayer = player1;
         }
-        console.log("after switching turn"+turnOfPlayer.getInternalName());
     }
 
     const getPlayerOnTurn = () => turnOfPlayer;
-    
-    
-    return { endOfGame, turnOfPlayer, switchTurn, getPlayerOnTurn };
+
+    const checkEndOfGame = () => {
+        let marks = ["", "", ""];
+        let currentMark = turnOfPlayer.getMark();
+        for (let y = 0; y < 3; y++) {
+            for (let x = 0; x < 3; x++) {
+                marks[x] = gameboard.showGameboard(y, x);
+            }
+            if (marks[0] == currentMark && marks[1] == currentMark && marks[2] == currentMark) {
+                endOfGame = true;
+            }
+        }
+        console.log("between the loops " + endOfGame);
+        if (endOfGame == false) {
+            for (let x = 0; x < 3; x++) {
+                console.log("in the second loop " + endOfGame);
+                for (let y = 0; y < 3; y++) {
+                    marks[y] = gameboard.showGameboard(y, x);
+                }
+                console.log(marks);
+                if (marks[0] == currentMark && marks[1] == currentMark && marks[2] == currentMark) {
+                    endOfGame = true;
+                    console.log("in the loop " + endOfGame);
+                }
+            }
+        }
+    }
+
+
+    return { endOfGame, turnOfPlayer, switchTurn, getPlayerOnTurn, checkEndOfGame };
 })();
 
 const displayController = (() => {
     const renderGameboard = () => {
         let id = 1;
-        for (let y = 0; y < 3; y++){
-            for (let x = 0; x < 3; x++){
+        for (let y = 0; y < 3; y++) {
+            for (let x = 0; x < 3; x++) {
                 let mark = gameboard.showGameboard(y, x);
                 let indexString = "box" + id.toString();
                 const currentBox = document.querySelector(`#${indexString}`);
