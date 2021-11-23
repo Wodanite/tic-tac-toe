f0a1ffb643d46f379d136171e4c0de7d2edabac6
Filip <f.golub321@gmail.com>
2021-11-23 22:06:38 +0100

    Stop putting marks on fields if game is finished

--- scripts/main.js
+++ scripts/main.js
@@ -23,30 +23,36 @@ const gameboard = (() => {
         gameboardFields[y][x] = mark;
     };
 
-    for (let i = 1; i < 10; i++) {
-        let indexString = "box" + i.toString();
-        const currentBox = document.querySelector(`#${indexString}`);
-        currentBox.addEventListener("click", () => {
-            const list = currentBox.classList;
-            const [y, x] = getCoordinates(list);
-
-            if (gameboard.checkIfLegalMove(y, x) == true) {
-                gameboard.addMark(y, x);
-
-                displayController.renderGameboard();
+    const addFunctionalityToGrid = () => {
+        for (let i = 1; i < 10; i++) {
+            let indexString = "box" + i.toString();
+            const currentBox = document.querySelector(`#${indexString}`);
+            currentBox.addEventListener("click", function doStuffOnClick() {
                 const [isEndOfGame, isDraw] = gameFlow.checkEndOfGame();
-                console.log(isEndOfGame, isDraw);
                 if (isEndOfGame == false) {
-                    gameFlow.switchTurn();
-                } else if (isEndOfGame == true && isDraw == false) {
-                    displayController.announceWinner();
-                } else {
-                    displayController.announceDraw();
+                    const list = currentBox.classList;
+                    const [y, x] = getCoordinates(list);
+
+                    if (gameboard.checkIfLegalMove(y, x) == true) {
+                        gameboard.addMark(y, x);
+
+                        displayController.renderGameboard();
+                        const [isEndOfGame, isDraw] = gameFlow.checkEndOfGame();
+                        console.log(isEndOfGame, isDraw);
+                        if (isEndOfGame == false) {
+                            gameFlow.switchTurn();
+                        } else {
+                            if (isEndOfGame == true && isDraw == false) {
+                                displayController.announceWinner();
+                            } else {
+                                displayController.announceDraw();
+                            }
+                        }
+                    }
                 }
 
-            }
-
-        });
+            });
+        }
     }
 
     function getCoordinates(list) {
@@ -59,7 +65,7 @@ const gameboard = (() => {
         return [y, x];
     }
 
-    return { showGameboard, checkIfLegalMove, getCoordinates, addMark };
+    return { showGameboard, checkIfLegalMove, getCoordinates, addMark, addFunctionalityToGrid };
 })();
 
 const Player = (nameOnDisplay, mark, internalName) => {
@@ -73,7 +79,36 @@ const Player = (nameOnDisplay, mark, internalName) => {
 const player1 = Player("Player 1", "X", "player1");
 const player2 = Player("Player 2", "O", "player2");
 
+
+
+const displayController = (() => {
+    const renderGameboard = () => {
+        let id = 1;
+        for (let y = 0; y < 3; y++) {
+            for (let x = 0; x < 3; x++) {
+                let mark = gameboard.showGameboard(y, x);
+                let indexString = "box" + id.toString();
+                const currentBox = document.querySelector(`#${indexString}`);
+                currentBox.textContent = mark;
+                id++;
+            }
+        }
+    }
+
+    const announceWinner = () => {
+        console.log(`The winner is ${gameFlow.getPlayerOnTurn().getNameOnDisplay()}!`);
+    }
+
+    const announceDraw = () => {
+        console.log("It's a Draw!");
+    }
+
+    return { renderGameboard, announceWinner, announceDraw };
+})();
+
 const gameFlow = (() => {
+    displayController.renderGameboard();
+    gameboard.addFunctionalityToGrid();
     let turnOfPlayer = player1;
     const switchTurn = () => {
         if (turnOfPlayer.getInternalName() == "player1") {
@@ -145,30 +180,3 @@ const gameFlow = (() => {
 
     return { turnOfPlayer, switchTurn, getPlayerOnTurn, checkEndOfGame };
 })();
-
-const displayController = (() => {
-    const renderGameboard = () => {
-        let id = 1;
-        for (let y = 0; y < 3; y++) {
-            for (let x = 0; x < 3; x++) {
-                let mark = gameboard.showGameboard(y, x);
-                let indexString = "box" + id.toString();
-                const currentBox = document.querySelector(`#${indexString}`);
-                currentBox.textContent = mark;
-                id++;
-            }
-        }
-    }
-
-    const announceWinner = () => {
-        console.log(`The winner is ${gameFlow.getPlayerOnTurn().getNameOnDisplay()}!`);
-    }
-
-    const announceDraw = () => {
-        console.log("It's a Draw!");
-    }
-
-    return { renderGameboard, announceWinner, announceDraw };
-})();
-
-displayController.renderGameboard();
